b1ad142c6fee0a068e180f4aad742ead3fc10a81
Weronika <weronikaa1@gmail.com>
2020-09-04 09:12:34 +0200

    zad4 - Added admin route and action (listing/index) for recommendations grid, added dummy test header for new action

---
+++ app/code/Bulbulatory/Recommendations/Block/Adminhtml/ListHeader.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Bulbulatory\Recommendations\Block\Adminhtml;
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * Class ListHeader
+ * @package Bulbulatory\Recommendations\Block\Adminhtml
+ */
+class ListHeader extends Template
+{
+}

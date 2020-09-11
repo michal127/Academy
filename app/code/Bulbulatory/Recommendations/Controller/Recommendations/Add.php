ccacf7cd8cfec136be67dc939498e5ca6d482654
michal127 <mic127@onet.eu>
2020-09-11 15:02:13 +0200

    zad8 - Sending simple recommendation email

--- app/code/Bulbulatory/Recommendations/Controller/Recommendations/Add.php
+++ app/code/Bulbulatory/Recommendations/Controller/Recommendations/Add.php
@@ -4,11 +4,14 @@
 namespace Bulbulatory\Recommendations\Controller\Recommendations;
 
 
+use Bulbulatory\Recommendations\Helper\Email;
 use Bulbulatory\Recommendations\Model\RecommendationRepository;
+use Exception;
 use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\App\Action\HttpPostActionInterface as HttpPostActionInterface;
 use Magento\Framework\Controller\ResultInterface;
+use Magento\Framework\UrlInterface;
 use Ramsey\Uuid\Uuid;
 
 /**
@@ -17,13 +20,25 @@ use Ramsey\Uuid\Uuid;
  */
 class Add extends LoggedInAction implements HttpPostActionInterface
 {
+    const XML_PATH_EMAIL_TEMPLATE_FIELD = 'recommendations/general/recommendation_email';
+
     /**
      * @var RecommendationRepository
      */
     private $recommendationsRepository;
+    /**
+     * @var Email
+     */
+    private $emailHelper;
+    /**
+     * @var UrlInterface
+     */
+    private $urlBuilder;
 
-    public function __construct(Context $context, Session $customerSession, RecommendationRepository $recommendationRepository)
+    public function __construct(Context $context, Session $customerSession, RecommendationRepository $recommendationRepository, Email $emailHelper, UrlInterface $urlBuilder)
     {
+        $this->emailHelper = $emailHelper;
+        $this->urlBuilder = $urlBuilder;
         $this->recommendationsRepository = $recommendationRepository;
         parent::__construct($context, $customerSession);
     }
@@ -46,10 +61,29 @@ class Add extends LoggedInAction implements HttpPostActionInterface
         } else {
             $hash = Uuid::uuid4();
             if ($this->recommendationsRepository->createRecommendation($customer, $recommendedEmail, $hash)) {
+                try {
+                    $receiverInfo = [
+                        'name' => $recommendedEmail,
+                        'email' => $recommendedEmail
+                    ];
 
-                //TODO send email
-                $this->messageManager->addSuccessMessage(__('Recommendation send successfully!'));
-            }else{
+                    $senderInfo = [
+                        'name' => 'Bulbulatory',
+                        'email' => 'bulbulatory@magento.pl',
+                    ];
+
+                    $templateVars = [
+                        'recommendedEmail' => $recommendedEmail,
+                        'confirmationUrl' => $this->urlBuilder->getUrl('recommendations/action/confirm', ['hash' => base64_encode($hash)]), //TODO add valid link
+                        'senderName' => $customer->getName(),
+                    ];
+
+                    $this->emailHelper->sendRecommendationEmail(self::XML_PATH_EMAIL_TEMPLATE_FIELD,$templateVars, $senderInfo, $receiverInfo);
+                    $this->messageManager->addSuccessMessage(__('Recommendation send successfully!'));
+                } catch (Exception $e) {
+                    $this->messageManager->addErrorMessage(__('An error occurred while sending recommendation. please try again.'));
+                }
+            } else {
                 $this->messageManager->addErrorMessage(__('An error occurred while sending recommendation. please try again.'));
             }
         }
---
+++ app/code/Bulbulatory/Recommendations/Helper/Email.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace Bulbulatory\Recommendations\Helper;
+
+use Magento\Framework\App\Area;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\MailException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Mail\Template\TransportBuilder;
+use Magento\Framework\Translate\Inline\StateInterface;
+use Magento\Store\Api\Data\StoreInterface;
+use Magento\Store\Model\ScopeInterface;
+use Magento\Store\Model\StoreManagerInterface;
+
+/**
+ * Class Email
+ * @package Bulbulatory\Recommendations\Helper
+ */
+class Email extends AbstractHelper
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    protected $_scopeConfig;
+
+    /**
+     * Store manager
+     *
+     * @var StoreManagerInterface
+     */
+    protected $_storeManager;
+
+    /**
+     * @var StateInterface
+     */
+    protected $inlineTranslation;
+
+    /**
+     * @var TransportBuilder
+     */
+    protected $_transportBuilder;
+
+    /**
+     * @var string
+     */
+    protected $temp_id;
+
+    /**
+     * @param Context $context
+     * @param StoreManagerInterface $storeManager
+     * @param StateInterface $inlineTranslation
+     * @param TransportBuilder $transportBuilder
+     */
+    public function __construct(
+        Context $context,
+        StoreManagerInterface $storeManager,
+        StateInterface $inlineTranslation,
+        TransportBuilder $transportBuilder
+    )
+    {
+        $this->_scopeConfig = $context;
+        parent::__construct($context);
+        $this->_storeManager = $storeManager;
+        $this->inlineTranslation = $inlineTranslation;
+        $this->_transportBuilder = $transportBuilder;
+    }
+
+    /**
+     * Return store configuration
+     *
+     * @param string $path
+     * @param int $storeId
+     * @return mixed
+     */
+    protected function getConfigValue(string $path, int $storeId)
+    {
+        return $this->scopeConfig->getValue(
+            $path,
+            ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+    }
+
+    /**
+     * Return store
+     *
+     * @return StoreInterface
+     * @throws NoSuchEntityException
+     */
+    public function getStore()
+    {
+        return $this->_storeManager->getStore();
+    }
+
+    /**
+     * Return template id according to store
+     *
+     * @param $xmlPath
+     * @return mixed
+     * @throws NoSuchEntityException
+     */
+    public function getTemplateId(string $xmlPath)
+    {
+        return $this->getConfigValue($xmlPath, $this->getStore()->getStoreId());
+    }
+
+    /**
+     * Generate email template
+     * @param array $emailTemplateVariables
+     * @param array $senderInfo
+     * @param array $receiverInfo
+     * @return Email
+     * @throws NoSuchEntityException
+     */
+    public function generateTemplate(array $emailTemplateVariables, array $senderInfo, array $receiverInfo)
+    {
+        $this->_transportBuilder->setTemplateIdentifier($this->temp_id)
+            ->setTemplateOptions(
+                [
+                    'area' => Area::AREA_FRONTEND,
+                    'store' => $this->_storeManager->getStore()->getId(),
+                ]
+            )
+            ->setTemplateVars($emailTemplateVariables)
+            ->setFrom($senderInfo)
+            ->addTo($receiverInfo['email'], $receiverInfo['name']);
+        return $this;
+    }
+
+    /**
+     * Sending recommendation email
+     * @param string $emailTemplateID
+     * @param array $emailTemplateVariables
+     * @param array $senderInfo
+     * @param array $receiverInfo
+     * @throws LocalizedException
+     * @throws MailException
+     * @throws NoSuchEntityException
+     */
+    public function sendRecommendationEmail(string $emailTemplateID, array $emailTemplateVariables, array $senderInfo, array $receiverInfo)
+    {
+        $this->temp_id = $this->getTemplateId($emailTemplateID);
+        $this->inlineTranslation->suspend();
+        $this->generateTemplate($emailTemplateVariables, $senderInfo, $receiverInfo);
+        $transport = $this->_transportBuilder->getTransport();
+        $transport->sendMessage();
+        $this->inlineTranslation->resume();
+    }
+}

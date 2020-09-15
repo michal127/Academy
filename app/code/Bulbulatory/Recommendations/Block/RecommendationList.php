bf847567dc7da8cfb3f2b734491b5fecc81219bc
michal127 <mic127@onet.eu>
2020-09-15 18:10:20 +0200

    zad9 - Prepared block for customer recommendations table

---
+++ app/code/Bulbulatory/Recommendations/Block/RecommendationList.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace Bulbulatory\Recommendations\Block;
+
+
+use Magento\Framework\View\Element\Template;
+
+/**
+ * Class Index
+ * @package Bulbulatory\Recommendations\Block
+ */
+class RecommendationList extends Template
+{
+
+}
